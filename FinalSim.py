92b669485a968824da27f08b491c27e13b980ac6
AlexaWalker <alexa.walker@ontariotechu.net>
2023-04-11 18:33:31 -0400

    more updates

--- FinalSim.py
+++ FinalSim.py
@@ -21,14 +21,18 @@ HEIGHT = 350
 
 class Player():
     def __init__(self):
+        #random start position
         self.x = rand.randint(0, 620)
         self.y = rand.randint(0, 350)
+
+        #random starting velocity
         self.velocity = np.array((rand.randint(2, 5), rand.randint(2, 5)))
         self.tot_vel = math.sqrt(self.velocity[0]**2 + self.velocity[1]**2)
+
         self.state = [self.x, self.y, self.velocity[0], self.velocity[1]] #x_pos, y_pos, x_vel, y_vel
         self.dt = 1.0
         self.t = 0
-        self.mass = 200
+        self.mass = 90 #kg
         self.radius = 7
 
         self.solver = ode(self.f)
@@ -38,17 +42,14 @@ class Player():
     def draw(self, bg, colour):
         pygame.draw.circle(bg, colour, (self.x, self.y), self.radius)
 
-    def f(self, t, x, y, p2):
-        self.mass, p2.mass = self.mass, p2.mass
-        M = self.mass + p2.mass
-        r1, r2 = self.radius, p2.radius
-        d = np.linalg.norm(r1 - r2)**2
-        v1, v2 = self.tot_velocity, p2.tot_velocity
-        u1 = v1 - 2*p2.mass / M * np.dot(v1-v2, r1-r2) / d * (r1 - r2)
-        u2 = v2 - 2*self.mass / M * np.dot(v2-v1, r2-r1) / d * (r2 - r1)
-        self.v = u1
-        p2.v = u2
-        return[]
+    def f(self, p2):
+        v =  ((self.mass-p2.mass)/(self.mass+p2.mass))*self.tot_vel+((2*p2.mass)/(self.mass+p2.mass))*p2.tot_vel
+        theta = math.atan((p2.state[1]-self.state[1])/(p2.state[0]-self.state[0]))
+        vx = v*math.cos(theta)
+        vy = v*math.sin(theta)
+        x = self.state[0] + vx 
+        y = self.state[1] + vy
+        return[x, y, vx, vy]
     
     def update(self, p2): 
         new_state = self.solver.integrate(self.t + self.dt)
@@ -58,34 +59,34 @@ class Player():
             self.t += self.t
 
             #skating motion
-            self.x += self.velocity[0]
-            self.y += self.velocity[1]
+            self.state[0] += self.state[2]
+            self.state[1] += self.state[3]
 
             if self.x - self.radius < 0:
                 self.x = self.radius
-                self.velocity[0] = rand.randint(2, 5)
-                self.velocity[1] = rand.randint(0, 5)
-                self.velocity[1] *= -1
+                self.state[2] = rand.randint(2, 5)
+                self.state[3] = rand.randint(0, 5)
+                self.state[3] *= -1
 
-            elif self.x + self.radius >= WIDTH:
-                self.x = WIDTH - self.radius
-                self.velocity[0] = rand.randint(2, 5)
-                self.velocity[1] = rand.randint(0, 5)
-                self.velocity[0] *= -1
+            elif self.state[0] + self.radius >= WIDTH:
+                self.state[0] = WIDTH - self.radius
+                self.state[2] = rand.randint(2, 5)
+                self.state[3] = rand.randint(0, 5)
+                self.state[2] *= -1
 
-            if self.y - self.radius < 0:
-                self.y = 0 + self.radius
-                self.velocity[0] = rand.randint(0, 5)
-                self.velocity[1] = rand.randint(2, 5)
-                self.velocity[0] *= -1
+            if self.state[1] - self.radius < 0:
+                self.state[1] = 0 + self.radius
+                self.state[2] = rand.randint(0, 5)
+                self.state[3] = rand.randint(2, 5)
+                self.state[2] *= -1
 
-            elif self.y + self.radius >= HEIGHT:
-                self.y = HEIGHT - self.radius
-                self.velocity[0] = rand.randint(0, 5)
-                self.velocity[1] = rand.randint(2, 5)
-                self.velocity[1] *= -1
+            elif self.state[1] + self.radius >= HEIGHT:
+                self.state[1] = HEIGHT - self.radius
+                self.state[2] = rand.randint(0, 5)
+                self.state[3] = rand.randint(2, 5)
+                self.state[3] *= -1
 
-            self.tot_vel = math.sqrt(self.velocity[0]**2 + self.velocity[1]**2)
+            self.tot_vel = math.sqrt(self.state[1]**2 + self.state[3]**2)
 
         else:
             state_after_collision, collision_time = self.collide(new_state, p2, self.t+self.dt)
@@ -94,58 +95,62 @@ class Player():
             self.solver.set_intial_value(self.state, self.t)
 
 
-        def check_collision(self, state, p2):
-            return (math.sqrt((p2.state[0]-state[0])**2+(p2.state[1]-state[1])**2)<=self.radius+p2.radius)
+    def check_collision(self, state, p2):
+        return (math.sqrt((p2.state[0]-state[0])**2+(p2.state[1]-state[1])**2)<=self.radius+p2.radius)
         
-        def collide(self, state, p2, time):
+    def collide(self, state, p2, time):
+        pass
             
 
-def close_pass(p1, p2):
-    return (math.sqrt((p2.x-p1.x)**2+(p2.y-p1.y)**2)<=p1.radius+p2.radius+10)
+    def close_pass(self, p2):
+        return (math.sqrt((p2.state[0]-self.state[0])**2+(p2.state[1]-self.state[1])**2)<=self.radius+p2.radius+10)
 
 
-def stay_in_area(p, x_min, x_max, y_min, y_max):
-    if p.x - p.radius < x_min:
-        p.x = p.radius
-        p.velocity[0] = 2
-        p.velocity[1] = 2
-        p.velocity[1] *= -1
+    def stay_in_area(self, x_min, x_max, y_min, y_max):
+        if self.x - self.radius < x_min:
+            self.x = self.radius
+            self.state[2] = 2
+            self.state[3] = 2
+            self.state[3] *= -1
 
-    elif p.x + p.radius >= x_max:
-        p.x = WIDTH - p.radius
-        p.velocity[0] = rand.randint(2, 5)
-        p.velocity[1] = rand.randint(0, 5)
-        p.velocity[0] *= -1
+        elif self.state[0] + self.radius >= x_max:
+            self.state[0] = WIDTH - self.radius
+            self.state[2] = rand.randint(2, 5)
+            self.state[3] = rand.randint(0, 5)
+            self.state[2] *= -1
 
-    if p.y - p.radius < y_min:
-        p.y = 0 + p.radius
-        p.velocity[0] = rand.randint(0, 5)
-        p.velocity[1] = rand.randint(2, 5)
-        p.velocity[0] *= -1
+        if self.state[1] - self.radius < y_min:
+            self.state[1] = 0 + self.radius
+            self.state[2] = rand.randint(0, 5)
+            self.state[3] = rand.randint(2, 5)
+            self.state[2] *= -1
 
-    elif p.y + p.radius >= y_max:
-        p.y = HEIGHT - p.radius
-        p.velocity[0] = rand.randint(0, 5)
-        p.velocity[1] = rand.randint(2, 5)
-        p.velocity[1] *= -1
-
-def fight(p1, p2):
-    fight_area_x_min = p1.x - 20
-    fight_area_x_max = p1.x + 20
-    fight_area_y_min = p1.y - 20
-    fight_area_y_max = p1.y + 20
-
-    stay_in_area(p1, fight_area_x_min, fight_area_x_max, fight_area_y_min, fight_area_y_max)
-    if check_collision(p1, p2):
-        collide(p1, p2)
-    else:
-        pass
-
-def track(p1, p2):
-    if(close_pass(p1, p2)):
-        fight(p1, p2)
+        elif self.state[1] + self.radius >= y_max:
+            self.state[1] = HEIGHT - self.radius
+            self.state[2] = rand.randint(0, 5)
+            self.state[3] = rand.randint(2, 5)
+            self.state[3] *= -1
         
+        self.tot_vel = math.sqrt(self.state[2]**2 + self.state[3]**2)
 
+    def fight(self, p2):
+        fight_area_x_min = self.x - 20
+        fight_area_x_max = self.x + 20
+        fight_area_y_min = self.y - 20
+        fight_area_y_max = self.y + 20
+
+        self.stay_in_area(fight_area_x_min, fight_area_x_max, fight_area_y_min, fight_area_y_max)
+        if self.check_collision(p2):
+            self.collide(p2, self.t)
+        else:
+            pass
+
+
+    def track(self, p1, p2):
+        if(self.close_pass(p2)):
+            self.fight(p2)
+
+        
 
 def Main():
     clock = pygame.time.Clock()
@@ -168,10 +173,10 @@ def Main():
             pygame.quit()
             sys.exit(0)
 
-        player1.update()
-        player2.update()
-        track(player1, player2)
-        check_collision(player1, player2)
+        player1.update(player2)
+        player2.update(player1)
+        player1.track(player2)
+        player1.check_collision(player2)
         screen.blit(bg, (0,0))
         player1.draw(screen, VGKGOLD)
         player2.draw(screen, TORBLUE)
