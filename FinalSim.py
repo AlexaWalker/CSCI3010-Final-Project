5c2faeb8f13c5433b2b657d79607dd35786bb116
AlexaWalker <alexa.walker@ontariotechu.net>
2023-04-11 15:19:42 -0400

    .

--- FinalSim.py
+++ FinalSim.py
@@ -25,63 +25,84 @@ class Player():
         self.y = rand.randint(0, 350)
         self.velocity = np.array((rand.randint(2, 5), rand.randint(2, 5)))
         self.tot_vel = math.sqrt(self.velocity[0]**2 + self.velocity[1]**2)
-        #self.state = [x, y, velocity[0], velocity[1]] #x_pos, y_pos, x_vel,.velocity[1]
+        self.state = [self.x, self.y, self.velocity[0], self.velocity[1]] #x_pos, y_pos, x_vel, y_vel
         self.dt = 1.0
         self.t = 0
         self.mass = 200
         self.radius = 7
 
+        self.solver = ode(self.f)
+        self.solver.set_integrator('dop853')
+        self.solver.set_initial_value(self.state, self.t)
+
     def draw(self, bg, colour):
         pygame.draw.circle(bg, colour, (self.x, self.y), self.radius)
+
+    def f(self, t, x, y, p2):
+        self.mass, p2.mass = self.mass, p2.mass
+        M = self.mass + p2.mass
+        r1, r2 = self.radius, p2.radius
+        d = np.linalg.norm(r1 - r2)**2
+        v1, v2 = self.tot_velocity, p2.tot_velocity
+        u1 = v1 - 2*p2.mass / M * np.dot(v1-v2, r1-r2) / d * (r1 - r2)
+        u2 = v2 - 2*self.mass / M * np.dot(v2-v1, r2-r1) / d * (r2 - r1)
+        self.v = u1
+        p2.v = u2
+        return[]
     
-    def update(self): 
-        #skating motion
-        self.x += self.velocity[0]
-        self.y += self.velocity[1]
+    def update(self, p2): 
+        new_state = self.solver.integrate(self.t + self.dt)
 
-        if self.x - self.radius < 0:
-            self.x = self.radius
-            self.velocity[0] = rand.randint(2, 5)
-            self.velocity[1] = rand.randint(0, 5)
-            self.velocity[1] *= -1
+        if not self.check_collision(new_state):
+            self.state = new_state
+            self.t += self.t
 
-        elif self.x + self.radius >= WIDTH:
-            self.x = WIDTH - self.radius
-            self.velocity[0] = rand.randint(2, 5)
-            self.velocity[1] = rand.randint(0, 5)
-            self.velocity[0] *= -1
+            #skating motion
+            self.x += self.velocity[0]
+            self.y += self.velocity[1]
 
-        if self.y - self.radius < 0:
-            self.y = 0 + self.radius
-            self.velocity[0] = rand.randint(0, 5)
-            self.velocity[1] = rand.randint(2, 5)
-            self.velocity[0] *= -1
+            if self.x - self.radius < 0:
+                self.x = self.radius
+                self.velocity[0] = rand.randint(2, 5)
+                self.velocity[1] = rand.randint(0, 5)
+                self.velocity[1] *= -1
 
-        elif self.y + self.radius >= HEIGHT:
-            self.y = HEIGHT - self.radius
-            self.velocity[0] = rand.randint(0, 5)
-            self.velocity[1] = rand.randint(2, 5)
-            self.velocity[1] *= -1
+            elif self.x + self.radius >= WIDTH:
+                self.x = WIDTH - self.radius
+                self.velocity[0] = rand.randint(2, 5)
+                self.velocity[1] = rand.randint(0, 5)
+                self.velocity[0] *= -1
 
-        self.tot_vel = math.sqrt(self.velocity[0]**2 + self.velocity[1]**2)
+            if self.y - self.radius < 0:
+                self.y = 0 + self.radius
+                self.velocity[0] = rand.randint(0, 5)
+                self.velocity[1] = rand.randint(2, 5)
+                self.velocity[0] *= -1
+
+            elif self.y + self.radius >= HEIGHT:
+                self.y = HEIGHT - self.radius
+                self.velocity[0] = rand.randint(0, 5)
+                self.velocity[1] = rand.randint(2, 5)
+                self.velocity[1] *= -1
+
+            self.tot_vel = math.sqrt(self.velocity[0]**2 + self.velocity[1]**2)
+
+        else:
+            state_after_collision, collision_time = self.collide(new_state, p2, self.t+self.dt)
+            self.state = state_after_collision
+            self.t = collision_time
+            self.solver.set_intial_value(self.state, self.t)
 
 
-def check_collision(p1, p2):
-    return (math.sqrt((p2.x-p1.x)**2+(p2.y-p1.y)**2)<=p1.radius+p2.radius)
+        def check_collision(self, state, p2):
+            return (math.sqrt((p2.state[0]-state[0])**2+(p2.state[1]-state[1])**2)<=self.radius+p2.radius)
+        
+        def collide(self, state, p2, time):
+            
 
 def close_pass(p1, p2):
     return (math.sqrt((p2.x-p1.x)**2+(p2.y-p1.y)**2)<=p1.radius+p2.radius+10)
 
-def collide(p1, p2):
-    p1.mass, p2.mass = p1.radius**2, p2.radius**2
-    M = p1.mass + p2.mass
-    r1, r2 = p1.radius, p2.radius
-    d = np.linalg.norm(r1 - r2)**2
-    v1, v2 = p1.velocity, p2.velocity
-    u1 = v1 - 2*p2.mass / M * np.dot(v1-v2, r1-r2) / d * (r1 - r2)
-    u2 = v2 - 2*p1.mass / M * np.dot(v2-v1, r2-r1) / d * (r2 - r1)
-    p1.v = u1
-    p2.v = u2
 
 def stay_in_area(p, x_min, x_max, y_min, y_max):
     if p.x - p.radius < x_min:
